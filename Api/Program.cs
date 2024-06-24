7e9705d30fe0f073958659c2427d884a801984a5
sistemacom4 <sistema@com4.com.br>
2024-06-24 17:44:24 -0300

    refac: commiting other files

---
+++ Api/Program.cs
@@ -0,0 +1,26 @@
+using CrossCutting.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
